818f5d2a41af6da2d003c8729b013e7e2b10106d
Gutoneitzke <gutoneitzke12345@gmail.com>
2024-01-17 22:53:32 -0300

    feat: new updates

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const hoursElement   = document.getElementById('hours')
 const minutesElement = document.getElementById('minutes')
 const secondsElement = document.getElementById('seconds')
 
-setInterval(() => {
+
+function fixTime(time){
+    return time < 10 ? '0'+time : time
+}
+
+function newTime(){
     const date    = new Date()
     const hours   = date.getHours()
     const minutes = date.getMinutes()
@@ -11,8 +16,7 @@ setInterval(() => {
     hoursElement.textContent   = fixTime(hours)
     minutesElement.textContent = fixTime(minutes)
     secondsElement.textContent = fixTime(seconds)
-}, 1000)
-
-function fixTime(time){
-    return time < 10 ? '0'+time : time
 }
+
+newTime()
+setInterval(newTime, 1000)
